5ee5cea067b4b34b60501436c1b8b9534d2907c5
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-09-04 17:31:02 +0100

    Terminnado o projeto

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
 const optionImages = document.querySelectorAll(".option-image")
 
 
+function handleOptionClick(event) {
+
+    console.log("event")
+}
+
+
 
 optionImages.forEach( img => {
-    
-})
+
+img.addEventListener("click", handleOptionClick)
+
+} )
+
+
+
+
+
 
 
